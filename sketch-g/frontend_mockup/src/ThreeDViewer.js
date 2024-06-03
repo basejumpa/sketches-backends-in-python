c8b828b2ed87ef07b027bc89963a288d98e31514
basejumpa <alexander.mannwahrenberg@gmail.com>
2024-06-03 13:47:26 +0200

    Add primitive vertex selection

--- sketch-g/frontend_mockup/src/ThreeDViewer.js
+++ sketch-g/frontend_mockup/src/ThreeDViewer.js
@@ -1,10 +1,10 @@
 // src/ThreeDViewer.js
-import React, { useRef } from 'react';
-import { Canvas, useFrame } from '@react-three/fiber';
+import React, { useRef, useState } from 'react';
+import { Canvas, useFrame, extend, useThree } from '@react-three/fiber';
 import { OrbitControls, useGLTF } from '@react-three/drei';
 import * as THREE from 'three';
 
-function Model() {
+function Model({ onVertexSelect }) {
   const { scene } = useGLTF('/model.glb');
   const ref = useRef();
 
@@ -18,15 +18,46 @@ function Model() {
     }
   });
 
-  return <primitive object={scene} ref={ref} />;
+  const handlePointerDown = (event) => {
+    const mesh = event.object;
+    const point = event.point;
+
+    const vertices = mesh.geometry.attributes.position.array;
+    let closestVertex = null;
+    let closestDistance = Infinity;
+
+    for (let i = 0; i < vertices.length; i += 3) {
+      const vertex = new THREE.Vector3(vertices[i], vertices[i + 1], vertices[i + 2]);
+      const distance = vertex.distanceTo(point);
+      if (distance < closestDistance) {
+        closestVertex = vertex;
+        closestDistance = distance;
+      }
+    }
+
+    if (closestVertex) {
+      onVertexSelect(closestVertex);
+    }
+  };
+
+  return (
+    <primitive object={scene} ref={ref} onPointerDown={handlePointerDown} />
+  );
 }
 
 function ThreeDViewer() {
+  const [selectedVertex, setSelectedVertex] = useState(null);
+
+  const handleVertexSelect = (vertex) => {
+    setSelectedVertex(vertex);
+    console.log('Selected Vertex:', vertex);
+  };
+
   return (
     <Canvas style={{ height: '100vh' }}>
       <ambientLight intensity={0.5} />
       <pointLight position={[10, 10, 10]} />
-      <Model />
+      <Model onVertexSelect={handleVertexSelect} />
       <OrbitControls />
     </Canvas>
   );
