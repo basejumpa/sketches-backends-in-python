2616bdf43d710b18f38892b8bed8d019ece865e4
basejumpa <alexander.mannwahrenberg@gmail.com>
2024-06-04 16:08:49 +0200

    Add rotation, position, scaling

--- sketch-g/frontend_mockup/src/components/ThreeDViewer.js
+++ sketch-g/frontend_mockup/src/components/ThreeDViewer.js
@@ -4,15 +4,20 @@ import { OrbitControls, useGLTF } from '@react-three/drei';
 
 function Model() {
     const gltf = useGLTF('model.glb');
-    return <primitive object={gltf.scene} />;
+
+    return <primitive object={gltf.scene}
+                rotation={[ -Math.PI/2, 0, Math.PI/2]}
+                position={[0, -1, 0]}
+                scale={[2, 2, 2]}
+            />;
 }
 
 function ThreeDViewer() {
     return (
         <div>
             <Canvas style={{ height: '100vh' }}>
-                <Model />
-                <OrbitControls />
+                <Model/>
+                <OrbitControls/>
             </Canvas>
         </div>
     );
