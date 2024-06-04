293c6bbf794e83c599befc1d58f61d9d9c6f345e
basejumpa <alexander.mannwahrenberg@gmail.com>
2024-06-04 16:17:15 +0200

    Restrict horizontal rotation to 360°

--- sketch-g/frontend_mockup/src/components/ThreeDViewer.js
+++ sketch-g/frontend_mockup/src/components/ThreeDViewer.js
@@ -6,9 +6,9 @@ function Model() {
     const gltf = useGLTF('model.glb');
 
     return <primitive object={gltf.scene}
-                rotation={[ -Math.PI/2, 0, Math.PI/2]}
-                position={[0, -1, 0]}
-                scale={[2, 2, 2]}
+                rotation={[ -Math.PI/2, 0, Math.PI/2 ]}
+                position={[ 0, -1, 0 ]}
+                scale={[ 2, 2, 2 ]}
             />;
 }
 
@@ -17,7 +17,10 @@ function ThreeDViewer() {
         <div>
             <Canvas style={{ height: '100vh' }}>
                 <Model/>
-                <OrbitControls/>
+                <OrbitControls
+                    minAzimuthAngle={ -Math.PI }
+                    maxAzimuthAngle={ Math.PI }
+                />
             </Canvas>
         </div>
     );
